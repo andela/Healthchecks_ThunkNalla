42959bd9990c3ccfa84ad5b6542ea0511af8712c
victoriaaoka <victoria.aoka@andela.com>
2017-05-18 09:17:24 +0300

    [Feature #145028639] Add assertion for the contents of the response.

--- hc/accounts/tests/test_switch_team.py
+++ hc/accounts/tests/test_switch_team.py
@@ -11,10 +11,10 @@ class SwitchTeamTestCase(BaseTestCase):
         self.client.login(username="bob@example.org", password="password")
 
         url = "/accounts/switch_team/%s/" % self.alice.username
-        r = self.client.get(url, follow=True)
-        # print (r)
-
-        ### Assert the contents of r
+        #import ipdb; ipdb.set_trace()
+        r= self.client.get(url, follow=True)
+        self.assertIn("healthchecks.io - Monitor Cron Jobs", r.content)
+        ### **Assert the contents of r **
 
 
     def test_it_checks_team_membership(self):
