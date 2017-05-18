3855e0d84ca349396156e5c181ef175f27e097d0
georgreen <georgreen.ngunga@andela.com>
2017-05-18 23:45:14 +0300

    [#145028641] Chore: Merged branch badkinds and Channels access - Merged Branches to have one that present story on pivotal tracker - All changes on this story will be merge to this branch hence forth

--- hc/front/tests/test_add_channel.py
+++ hc/front/tests/test_add_channel.py
@@ -39,48 +39,27 @@ class AddChannelTestCase(BaseTestCase):
 
 class TeamAccessTestCase(BaseTestCase):
     # Test that the team access works
-    def test_team_access_works(self):
-        self.channel = Channel(user=self.alice, kind="email")
-        self.channel.value = "test that team access works"
-        self.channel.save()
+    def test_team_access_works_email(self):
+        kinds = ("email", "webhook", "pd", "pushover", "hipchat", "victorops")
+        for kind_name in kinds:
+            self.channel = Channel(user=self.alice, kind=kind_name)
+            self.channel.value = "test that team access works"
+            self.channel.save()
 
-        url = "/integrations/{}/checks/".format(self.channel.code)
+            url = "/integrations/{}/checks/".format(self.channel.code)
 
-        self.client.login(username="bob@example.org", password="password")
-        self.response = self.client.get(url)
-        self.assertEquals(self.response.status_code, 200)
+            self.client.login(username="bob@example.org", password="password")
+            self.response = self.client.get(url)
+            self.assertEquals(self.response.status_code, 200)
 
 
 # Test that bad kinds don't work//channels that raise Not implemented error
 class BadKindTestCase(BaseTestCase):
-
     def test_bad_kind(self):
         self.client.login(username="alice@example.org", password="password")
-        bad_kinds = ('0798885255', 'twitter', 'instagram', 'whatsapp', 'telegram', 'facebook', 'reddit')
+        bad_kinds = ('0798885255', 'twitter', 'instagram', 'whatsapp',
+                     'telegram', 'facebook', 'reddit')
         for any_kind in bad_kinds:
             url = "/integrations/add_%s/" % any_kind
             r = self.client.get(url)
             assert r.status_code == 404
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> origin/ft-testing-bad-kinds-#145028641
