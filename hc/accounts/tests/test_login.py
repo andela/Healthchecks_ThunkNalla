322b4d97e90cd0bf01a2a0a1adad13389553383c
victoriaaoka <victoria.aoka@andela.com>
2017-05-22 14:44:44 +0300

    [feature 145028639] Fixes assertion for email content from static to dynamic

--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from django.core import mail
-from django.test import TestCase
+from hc.test import BaseTestCase
 from hc.api.models import Check
 
 
-class LoginTestCase(TestCase):
+
+class LoginTestCase(BaseTestCase):
 
     def test_it_sends_link(self):
         check = Check()
@@ -12,24 +13,28 @@ class LoginTestCase(TestCase):
         session = self.client.session
         session["welcome_code"] = str(check.code)
         session.save()
-
         form = {"email": "alice@example.org"}
-
         r = self.client.post("/accounts/login/", form)
         assert r.status_code == 302
-        ### **Assert that a user was created
+
+        # ## **Assert that a user was created
         final_list = len(User.objects.all())
-        self.assertEqual(1, final_list)
+        self.assertEqual(3, final_list)
+
         # And email sent
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Log in to healthchecks.io')
-        ### Assert contents of the email body
-        self.assertIn("please open the link below:", mail.outbox[0].body)
-        ### Assert that check is associated with the new user
+
+        # ## Assert contents of the email body
+        self.assertIn("\n\nhttp://localhost:8000/accounts/check_token/" + self.profile.token, mail.outbox[0].body)
+
+
+        # ## Assert that check is associated with the new user
+
 
     def test_it_pops_bad_link_from_session(self):
         self.client.session["bad_link"] = True
         self.client.get("/accounts/login/")
         assert "bad_link" not in self.client.session
 
-        ### Any other tests?
+        # ## Any other tests?
