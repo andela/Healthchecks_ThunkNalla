02474d411bf9cd1a59fde645706818dd5c1297f9
Andretalik <adrianadero.otieno@gmail.com>
2017-05-22 11:52:56 +0300

    [Feature #145028639] Changed the output operator

--- hc/accounts/tests/test_switch_team.py
+++ hc/accounts/tests/test_switch_team.py
@@ -19,14 +19,14 @@ class SwitchTeamTestCase(BaseTestCase):
     def test_it_checks_team_membership(self):
         self.client.login(username="charlie@example.org", password="password")
 
-        url = "/accounts/switch_team/%s/" % self.alice.username
+        url = "/accounts/switch_team/{}/".format(self.alice.username)
         r = self.client.get(url)
         self.assertEqual(r.status_code, 403)
         ###** Assert the expected error code**
 
     def test_it_switches_to_own_team(self):
         self.client.login(username="alice@example.org", password="password")
-        url = "/accounts/switch_team/%s/" % self.alice.username
+        url = "/accounts/switch_team/{}/" .format(self.alice.username)
         r = self.client.get(url, follow=True)
         self.assertEqual(r.status_code, 200)
         ### **Assert the expected error code**
