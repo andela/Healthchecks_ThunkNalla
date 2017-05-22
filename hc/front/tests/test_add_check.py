dd8de877d6fa62fb1f584b505b5c79ee689a863f
georgreen <georgreen.ngunga@andela.com>
2017-05-22 14:46:19 +0300

    [#145028641] Implemented team access test in Checks - Added test for team access in checks feature

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -12,3 +12,18 @@ class AddCheckTestCase(BaseTestCase):
         assert Check.objects.count() == 1
 
     ### Test that team access works
+    def test_team_access(self):
+        self.check = Check(user=self.alice)
+        self.check.save()
+
+        self.client.login(username="bob@example.org", password="password")
+
+        url = "/checks/%s/timeout/" % self.check.code
+        payload = {"timeout": 5000, "grace": 60}
+
+        r = self.client.post(url, data=payload)
+        self.assertRedirects(r, "/checks/")
+
+        check = Check.objects.get(code=self.check.code)
+        assert check.timeout.total_seconds() == 5000
+        assert check.grace.total_seconds() == 60
