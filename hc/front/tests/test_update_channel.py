eb1953385d2aaacbe80a10a96a249bbed29e2244
georgreen <georgreen.ngunga@andela.com>
2017-05-24 23:25:15 +0300

    [#145028641] Chore: Refactored code for readability - Improved the variable naming style to be more descriptive - Pep 8 rules where also apllied where nessecary

--- hc/front/tests/test_update_channel.py
+++ hc/front/tests/test_update_channel.py
@@ -16,38 +16,38 @@ class UpdateChannelTestCase(BaseTestCase):
     def test_it_works(self):
         payload = {
             "channel": self.channel.code,
-            "check-%s" % self.check.code: True
+            "check-{}".format(self.check.code): True
         }
 
         self.client.login(username="alice@example.org", password="password")
-        r = self.client.post("/integrations/", data=payload)
-        self.assertRedirects(r, "/integrations/")
+        response = self.client.post("/integrations/", data=payload)
+        self.assertRedirects(response, "/integrations/")
 
         channel = Channel.objects.get(code=self.channel.code)
         checks = channel.checks.all()
-        assert len(checks) == 1
-        assert checks[0].code == self.check.code
+        self.assertEquals(len(checks), 1)
+        self.assertEquals(checks[0].code, self.check.code)
 
     def test_team_access_works(self):
         payload = {
             "channel": self.channel.code,
-            "check-%s" % self.check.code: True
+            "check-{}".format(self.check.code): True
         }
 
         # Logging in as bob, not alice. Bob has team access so this
         # should work.
         self.client.login(username="bob@example.org", password="password")
-        r = self.client.post("/integrations/", data=payload, follow=True)
-        self.assertEqual(r.status_code, 200)
+        response = self.client.post("/integrations/", data=payload, follow=True)
+        self.assertEqual(response.status_code, 200)
 
     def test_it_checks_channel_user(self):
         payload = {"channel": self.channel.code}
 
         self.client.login(username="charlie@example.org", password="password")
-        r = self.client.post("/integrations/", data=payload)
+        response = self.client.post("/integrations/", data=payload)
 
         # self.channel does not belong to charlie, this should fail--
-        assert r.status_code == 403
+        self.assertEquals(response.status_code, 403)
 
     def test_it_checks_check_user(self):
         charlies_channel = Channel(user=self.charlie, kind="email")
@@ -56,21 +56,21 @@ class UpdateChannelTestCase(BaseTestCase):
 
         payload = {
             "channel": charlies_channel.code,
-            "check-%s" % self.check.code: True
+            "check-{}".format(self.check.code): True
         }
         self.client.login(username="charlie@example.org", password="password")
-        r = self.client.post("/integrations/", data=payload)
+        response = self.client.post("/integrations/", data=payload)
 
         # mc belongs to charlie but self.check does not--
-        assert r.status_code == 403
+        self.assertEquals(response.status_code, 403)
 
     def test_it_handles_missing_channel(self):
         # Correct UUID but there is no channel for it:
         payload = {"channel": "6837d6ec-fc08-4da5-a67f-08a9ed1ccf62"}
 
         self.client.login(username="alice@example.org", password="password")
-        r = self.client.post("/integrations/", data=payload)
-        assert r.status_code == 400
+        response = self.client.post("/integrations/", data=payload)
+        self.assertEquals(response.status_code, 400)
 
     def test_it_handles_missing_check(self):
         # check- key has a correct UUID but there's no check object for it
@@ -80,5 +80,5 @@ class UpdateChannelTestCase(BaseTestCase):
         }
 
         self.client.login(username="alice@example.org", password="password")
-        r = self.client.post("/integrations/", data=payload)
-        assert r.status_code == 400
+        response = self.client.post("/integrations/", data=payload)
+        self.assertEquals(response.status_code, 400)
