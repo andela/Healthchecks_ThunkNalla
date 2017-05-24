1203a945267299ce96c97b4d921557f8696dba08
georgreen <georgreen.ngunga@andela.com>
2017-05-24 23:22:13 +0300

    [#145028641] Ft: Implemented more tests for Pushover - Added more test to test the pushover functionality - Test added covered a wider range includiong special cases and previous unseen scenarios

--- hc/front/tests/test_add_pushbover.py
+++ hc/front/tests/test_add_pushbover.py
@@ -13,19 +13,20 @@ class AddPushoverTestCase(BaseTestCase):
         session.save()
 
         params = "pushover_user_key=a&nonce=n&prio=0"
-        r = self.client.get("/integrations/add_pushover/?%s" % params)
-        assert r.status_code == 302
+        response = self.client.get("/integrations/add_pushover/?{}".
+                                   format(params))
+        self.assertEquals(response.status_code, 302)
 
         channels = list(Channel.objects.all())
-        assert len(channels) == 1
-        assert channels[0].value == "a|0"
+        self.assertEquals(len(channels), 1)
+        self.assertEquals(channels[0].value, "a|0")
 
     @override_settings(PUSHOVER_API_TOKEN=None)
     def test_it_requires_api_token(self):
         self.client.login(username="alice@example.org", password="password")
 
-        r = self.client.get("/integrations/add_pushover/")
-        self.assertEqual(r.status_code, 404)
+        response = self.client.get("/integrations/add_pushover/")
+        self.assertEqual(response.status_code, 404)
 
     def test_it_validates_nonce(self):
         self.client.login(username="alice@example.org", password="password")
@@ -35,11 +36,12 @@ class AddPushoverTestCase(BaseTestCase):
         session.save()
 
         params = "pushover_user_key=a&nonce=INVALID&prio=0"
-        r = self.client.get("/integrations/add_pushover/?%s" % params)
-        assert r.status_code == 403
+        response = self.client.get("/integrations/add_pushover/?{}".
+                                   format(params))
+        self.assertEquals(response.status_code, 403)
 
-    ### Test that pushover validates priority
-    def test_validate_priority(self):
+    # Test that pushover validates priority
+    def test_reject_wrong_priority(self):
         self.client.login(username="alice@example.org", password="password")
 
         session = self.client.session
@@ -47,7 +49,53 @@ class AddPushoverTestCase(BaseTestCase):
         session.save()
 
         priority_list = ['9', '-9', 'a', '#', 9, -9]
-        for i in priority_list:
-            params = "pushover_user_key=a&nonce=n&prio=%s" % i
-            r = self.client.get("/integrations/add_pushover/?%s" % params)
-            self.assertEqual(r.status_code, 400)
+        for each_priority in priority_list:
+            params = "pushover_user_key=a&nonce=n&prio={}".\
+                format(each_priority)
+            response = self.client.get("/integrations/add_pushover/?{}"
+                                       .format(params))
+            self.assertEquals(response.status_code, 400)
+
+    def test_priority_works_negative(self):
+        self.client.login(username="alice@example.org", password="password")
+
+        session = self.client.session
+        session["po_nonce"] = "n"
+        session.save()
+
+        priority = '-1'
+
+        params = "pushover_user_key=a&nonce=n&prio={}".format(priority)
+        response = self.client.get("/integrations/add_pushover/?{}"
+                                   .format(params))
+        self.assertEquals(response.status_code, 302)
+
+    def test_priority_works_positive(self):
+        self.client.login(username="bob@example.org", password="password")
+
+        session = self.client.session
+        session["po_nonce"] = "n"
+        session.save()
+
+        priority = '2'
+
+        params = "pushover_user_key=a&nonce=n&prio={}".format(priority)
+        response = self.client.get("/integrations/add_pushover/?{}"
+                                   .format(params))
+
+        self.assertEquals(response.status_code, 302)
+        self.assertRedirects(response, '/integrations/')
+
+    def test_priority_works_zero(self):
+        self.client.login(username="charlie@example.org", password="password")
+
+        session = self.client.session
+        session["po_nonce"] = "n"
+        session.save()
+
+        priority = '0'
+
+        params = "pushover_user_key=a&nonce=n&prio={}".format(priority)
+        response = self.client.get("/integrations/add_pushover/?{}"
+                                   .format(params))
+        self.assertRedirects(response, '/integrations/')
