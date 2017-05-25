085dcddd45593914e4247f9e08a343d8d7f4c1e0
georgreen <georgreen.ngunga@andela.com>
2017-05-25 22:16:48 +0300

    #145028641 Front tests  (#5)
    * [#145028641] FT: Add Test for Team Access
    - Tested if team members can access each other channels and profile
    
    * ft: Added test for bad kinds [#145028641]
    
    - We added a test for testing that only kinds specified work!
    
    * [#145028641] Chore: Merged branch badkinds and Channels access
    - Merged Branches to have one that present story on pivotal tracker
    - All changes on this story will be merge to this branch hence forth
    
    * Chore: Fixed typo in tests
    
    * [#145028641] Implemented team access test in Checks
    - Added test for team access in checks feature
    
    * [#145028641] Implemented test for priority validation.
    - Added a test for validating priority in pushover feature.
    
    * [#145028641] Fixed travis build error
    - Syntax Error caused by typo in add pushover
    
    * [#145028641] Chore: Removed Debug code
    - Removed unnessecary debug code from views.py
    
    * [##145028641] Ft: Fixed repetition in Tests
    - Improved test design by removing unnessecary code from the for loop
    
    * [#145028641] Chore: Refactored to improve readability
    - Gave variables more descriptive names to improve readability
    
    * [#145028641] Ft: Implemented more tests for Pushover
    - Added more test to test the pushover functionality
    - Test added covered a wider range includiong special cases and previous unseen scenarios
    
    * [#145028641] Chore: Refactored code for readability
    - Improved the variable naming style to be more descriptive
    - Pep 8 rules where also apllied where nessecary

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -7,8 +7,23 @@ class AddCheckTestCase(BaseTestCase):
     def test_it_works(self):
         url = "/checks/add/"
         self.client.login(username="alice@example.org", password="password")
-        r = self.client.post(url)
-        self.assertRedirects(r, "/checks/")
-        assert Check.objects.count() == 1
+        response = self.client.post(url)
+        self.assertRedirects(response, "/checks/")
+        self.assertEquals(Check.objects.count(), 1)
 
-    ### Test that team access works
+    # Test that team access works
+    def test_team_access(self):
+        self.check = Check(user=self.alice)
+        self.check.save()
+
+        self.client.login(username="bob@example.org", password="password")
+
+        url = "/checks/{}/timeout/".format(self.check.code)
+        payload = {"timeout": 5000, "grace": 60}
+
+        response = self.client.post(url, data=payload)
+        self.assertRedirects(response, "/checks/")
+
+        check = Check.objects.get(code=self.check.code)
+        self.assertEquals(check.timeout.total_seconds(), 5000)
+        self.assertEquals(check.grace.total_seconds(), 60)
