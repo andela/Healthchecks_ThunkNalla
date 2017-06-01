5e91a3ca876ce253013e8b661843fd8187f7cd8f
georgreen <georgreen.ngunga@andela.com>
2017-06-01 20:21:06 +0300

    [Feature #145028637] The API tests need fixing - Added test for send alerts feature - Added test for ping feature

--- hc/api/tests/test_sendalerts.py
+++ hc/api/tests/test_sendalerts.py
@@ -1,10 +1,11 @@
 from datetime import timedelta
 
 from django.utils import timezone
+from mock import patch
+
 from hc.api.management.commands.sendalerts import Command
 from hc.api.models import Check
 from hc.test import BaseTestCase
-from mock import patch
 
 
 class SendAlertsTestCase(BaseTestCase):
@@ -21,14 +22,14 @@ class SendAlertsTestCase(BaseTestCase):
             check.save()
 
         result = Command().handle_many()
-        assert result, "handle_many should return True"
+        self.assertTrue(result, "handle_many should return True")
 
         handled_names = []
         for args, kwargs in mock.call_args_list:
             handled_names.append(args[0].name)
 
-        assert set(names) == set(handled_names)
-        ### The above assert fails. Make it pass
+        self.assertTrue(set(names) == set(handled_names))
+        # The above assert fails. Make it pass
 
     def test_it_handles_grace_period(self):
         check = Check(user=self.alice, status="up")
@@ -36,7 +37,26 @@ class SendAlertsTestCase(BaseTestCase):
         check.last_ping = timezone.now() - timedelta(days=1, minutes=30)
         check.save()
         # Expect no exceptions--
-        Command().handle_one(check)
+        failed = False
+        try:
+            Command().handle_one(check)
+        except Exception:
+            failed = True
 
+        self.assertFalse(failed)
 
-    ### Assert when Command's handle many that when handle_many should return True
+    # Assert when Command's handle many that when handle_many should return
+    # True
+    @patch("hc.api.management.commands.sendalerts.Command.handle_many")
+    def test_it_handles_many(self, mock):
+        yesterday = timezone.now() - timedelta(days=1)
+        names = ["Check %d" % d for d in range(0, 10000)]
+
+        for name in names:
+            check = Check(user=self.alice, name=name)
+            check.alert_after = yesterday
+            check.status = "up"
+            check.save()
+
+        result = Command().handle_many()
+        self.assertTrue(result)
